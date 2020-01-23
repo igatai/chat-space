241c58737482970b263a004b9f97b724d0ce477a
igatai <taiti0614@gmail.com>
2020-01-23 16:37:08 +0900

    DisplayPostedMessageOnView

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
   def create
-    @message = @group.messages.new(mesage_params)
+    @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
