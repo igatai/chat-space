a4f28e76db36aea7d11aeb6fa752b5c0e8a13431
igatai <taiti0614@gmail.com>
2020-01-30 21:52:41 +0900

    commit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,79 @@
 $(function(){
 
-  $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();
-    // console.log(input);
+  //検索結果の追加
+  function addUser(user){
+      var html = `
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  </div>
+                  `;
+      $("#user-search-result").append(html);
+  }
 
-  })
+  // エラ〜メッセージの追加
+  function addErrMsgToHTML(msg){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addDeleteUser(name, id){
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+
+  // メンバーの追加
+  function addMember(user_id){
+    let html = `<input value="${user_id}" name="group[user_ids][]" type="hidden" id="group_user_ids_${user_id}" />`;
+    $(`#${user_id}`).append(html);
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url:  '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0 ){
+        users.forEach(function(user){
+          addUser(user);
+        });
+      } else if (input.length == 0 ){
+        return false;
+      } else {
+        addErrMsgToHTML("一致するユーザがいません");
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    });
+  });
+  $(document).on("click",".chat-group-user__btn--add",function(){
+    console.log("AAA");
+    const user_id = $(this).attr("data-user-id");
+    const user_name = $(this).attr("data-user-name");
+    console.log(user_id);
+    console.log(user_name);
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(user_name, user_id);
+    addMember(user_id);
+  });
+  $(document).on("click",".chat-group-user__btn--remove", function(){
+    $(this)
+      .parent()
+      .remove();
+  });
 });
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.search(input, id)
+    # binding.pry
+    return nil if input == ""
+    # モデル.where('カラム名 LIKE(?)',"%{:keyword}%")... :keywordを含み、自分自身を除くユーザー10件
+    User.where('name LIKE ?', "%#{input}%" ).where.not(id: id).limit(10)
+  end
 
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages
-
 end
